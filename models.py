bc3b03a4e5a448c86a701c12e8e062e418c1fd62
Ramnivash SK <115456745+Ramnivash123@users.noreply.github.com>
2025-05-19 12:48:13 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20)
-    gender = models.CharField(max_length=10)
-    state = models.CharField(max_length=10)
-    city = models.CharField(max_length=10)
+
 
 class State(models.Model):
     name = models.CharField(max_length=100)
@@ -16,3 +9,12 @@ class State(models.Model):
 class City(models.Model):
     name = models.CharField(max_length=100)
     state_id = models.ForeignKey(State, on_delete=models.CASCADE, null=True)
+
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    gender = models.CharField(max_length=10)
+    state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
